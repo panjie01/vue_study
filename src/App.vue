0e874701e1e28e5fc3741bedfe7f398207ca5c2b
pj <1306050161@qq.com>
2019-05-17 10:24:10 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <p>{{ message }}</p>
+        <p>{{detail()}}</p> 
+        <a :href='url'>联系我们 </a>
+        <p>{{info}}</p> <!-- 实现双向数据绑定 -->
+        <div>
+            <input type="text" v-model="info" />
+            <button v-on:click="handlerClick">添加</button>
+            <!-- <button @click="handlerClick">添加</button> -->
+        </div>
+        <ul>
+            <todo-item v-for="key in list"  :key="key">
+               <span slot="st" slot-scope="props" :style=" {fontSize: '20px',color:props.checked?'red':'blue'} ">{{key}}</span>
+               
+            </todo-item>
+
+            <!-- <todo-item v-for="key in list" :item='key' :key="key"></todo-item> -->
+        </ul>
+
+        <template v-if="info">
+          <h1>为梦想加油</h1>
+          <p>学的不仅是技术，更是梦想！</p>
+          <p>哈哈哈，打字辛苦啊！！！</p>
+        </template>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'app',
+  data() {
+      return {
+            message: 'Hello World!',
+            info:'',
+            list:[],
+            url: 'https://www.baidu.com'
+            // item2:'123'
+             }
+        },
+  methods:{
+            handlerClick() {
+                if(this.info=='' || this.info==null){
+                    alert('info can not be null')
+                    return
+                }
+                console.log(this.info)
+                this.list.push(this.info)
+                this.info=''
+            },
+            detail(){
+              return  "学的不仅是技术，更是梦想!";
+            } 
+        },
   components: {
-    HelloWorld
-  }
+    TodoItem
+  }      
 }
+
+
+
+
+
 </script>
 
 <style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <li class="cs">
+        <input type="checkbox" v-model="checked"/> 
+        <!-- <slot name="st">{{item}}</slot> -->
+        <!-- <slot name="st"></slot> -->
+        <slot name="st" v-bind="{checked}"></slot>
+    </li>
+</template>
+<script>
+    export default {
+        props:['item'], //声明能接受的参数（属性）
+        data() {
+            return{
+                checked:false
+            }
+        }
+    }
+</script>
+<style>
+    .cs {
+            color: red;
+        }
+
+</style>
